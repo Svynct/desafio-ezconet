2b95e68521580e6bf059a2e5c74175279c03539f
svynct <rtkiller006@gmail.com>
2021-10-08 23:40:02 -0300

    update testes

--- api-desafio/WebAPI.Testes/TestesAutomatizados.cs
+++ api-desafio/WebAPI.Testes/TestesAutomatizados.cs
@@ -1,116 +1,28 @@
-using Moq;
-using Shouldly;
-using System;
+using Microsoft.AspNetCore.Mvc.Testing;
 using System.Collections.Generic;
-using WebAPI.Application.Services.Implementation;
+using System.Net.Http;
 using WebAPI.Domain.Models;
-using WebAPI.Repository.Implementation;
 using Xunit;
 
 namespace WebAPI.Testes
 {
     public class TestesAutomatizados
     {
-        private readonly UsuarioService usuarioService;
+        private readonly WebApplicationFactory<Startup> _factory;
+        private readonly HttpClient _httpClient;
 
-        public TestesAutomatizados()
+        public TestesAutomatizados(WebApplicationFactory<Startup> factory)
         {
-            var repository = new MockRepository(MockBehavior.Strict) { DefaultValue = DefaultValue.Mock };
-            var mockFoo = repository.Create<UsuarioRepository>();
-            usuarioService = repository.Create<UsuarioService>(mockFoo.Object).Object;
+            _factory = factory;
+            _httpClient = _factory.CreateClient();
         }
 
         [Fact]
-        public void CriarUsuarioDeveRetornarMaiorQueZeroENaoDeveEstourarExcecao()
+        public async void RecuperarTodosUsuarios()
         {
-            Exception exception = null;
-
-            var usuario = new Usuario
-            {
-                Nome = "Teste",
-                Ativo = true,
-                DataNascimento = DateTime.Now,
-                Email = "teste@teste.com",
-                Senha = "1234",
-                SexoId = 1,
-            };
-
-            int rowsAffected = 0;
-
-            try
-            {
-                rowsAffected = usuarioService.CadastrarUsuario(usuario);
-            }
-            catch (Exception ex)
-            {
-                exception = ex;
-            }
-
-            exception.ShouldBeNull();
-            rowsAffected.ShouldBeGreaterThan(0);
-        }
-
-        [Fact]
-        public void RecuperarUsuariosDeveRecuperarUsuariosENaoDeveEstourarExcecao()
-        {
-            Exception exception = null;
-            List<UsuarioViewModel> usuarios = new List<UsuarioViewModel>();
-
-            var usuario = new Usuario
-            {
-                Nome = "Teste",
-                Ativo = true,
-                DataNascimento = DateTime.Now,
-                Email = "teste@teste.com",
-                Senha = "1234",
-                SexoId = 1,
-            };
-
-            usuarioService.CadastrarUsuario(usuario);
-
-            try
-            {
-                usuarios = usuarioService.RecuperarTodosUsuarios();
-            }
-            catch (Exception ex)
-            {
-                exception = ex;
-            }
-
-            exception.ShouldBeNull();
-            usuarios.ShouldNotBeEmpty();
-        }
-
-        [Fact]
-        public void DeletarUsuarioDeveRetornarMaiorQueZeroENaoDeveEstourarExcecao()
-        {
-            Exception exception = null;
-            int rowsAffected = 0;
-
-            var usuario = new Usuario
-            {
-                Nome = "Teste",
-                Ativo = true,
-                DataNascimento = DateTime.Now,
-                Email = "teste@teste.com",
-                Senha = "1234",
-                SexoId = 1,
-            };
-
-            usuarioService.CadastrarUsuario(usuario);
-
-            try
-            {
-                var u = usuarioService.RecuperarTodosUsuarios()[0];
-                rowsAffected = usuarioService.RemoverUsuario(u.UsuarioId);
-            }
-            catch (Exception ex)
-            {
-                exception = ex;
-            }
-
-            exception.ShouldBeNull();
-            rowsAffected.ShouldBeGreaterThan(0);
+            var result = await _httpClient.GetAsync("api/usuario");
+            Assert.NotNull(result.Content);
+            Assert.IsType<List<Usuario>>(result.Content);
         }
     }
 }
